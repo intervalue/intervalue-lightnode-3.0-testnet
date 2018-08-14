9d701095ec4aec5a3b2e700e90149ce3d3bb5a22
ylwhlhp <ylwhlhp@126.com>
2018-08-14 14:26:04 +0800

    update signature

--- src/js/controllers/signaturePubkey.js
+++ src/js/controllers/signaturePubkey.js
@@ -3,31 +3,38 @@ var db = require('intervaluecore/db');
 var shadowWallet = require('intervaluecore/shadowWallet');
 angular.module('copayApp.controllers').controller('signaturePubkeyControllers',
     function($scope, $rootScope, $timeout,go) {
-    var self = this;
+        var self = this;
 
         /**
          * 根据当前地址生成对应pubkey二维码
          * @param address
          */
-    self.generatePubkey = function (address) {
-        return self.generateVerificationQRCode = shadowWallet.getVerificationQRCode(address);
-    }
+
+        self.generatePubkey =  function (address) {
+            shadowWallet.getVerificationQRCode(address,function(verificationQRCode) {
+                alert(verificationQRCode);
+            });
+        }
 
         /**
          * 生成授权签名
          * @param verificationQRCode
          */
-    self.generateVerification = function(verificationQRCode){
-        return self.generateSignatureCode = shadowWallet.getSignatureCode(verificationQRCode);
-    }
+        self.generateVerification = function(verificationQRCode){
+            shadowWallet.getSignatureCode(verificationQRCode,function(signatureCode) {
+                alert(signatureCode);
+            });
+        }
 
 
         /**
          * 生成授权签名详情
          * @param signatureCode
          */
-    self.generateSignatureDetl = function(signatureCode){
-        return self.generateSignatureDetlCode = shadowWallet.getSignatureDetlCode(signatureCode);
-    }
+        self.generateSignatureDetl = function(signatureCode){
+            shadowWallet.getSignatureDetlCode(signatureCode,function(signatureDetlCode){
+                alert(signatureDetlCode);
+            });
+        }
 
     });
