daa9c32819b94e39da8a6f88ad4655a7d6ec5e95
dxl6868 <dxl6868@126.com>
2018-08-24 13:34:53 +0800

    交易同步前端

--- src/js/services/txFormatService.js
+++ src/js/services/txFormatService.js
@@ -2,34 +2,37 @@
 
 var constants = require('intervaluecore/constants.js');
 
-angular.module('copayApp.services').factory('txFormatService', function(profileService, configService, lodash) {
+angular.module('copayApp.services').factory('txFormatService', function (profileService, configService, lodash) {
   var root = {};
 
-  var formatAmountStr = function(amount, asset) {
-	if (!amount) return;
-	if (asset !== "base" && asset !==  constants.BLACKBYTES_ASSET && !profileService.assetMetadata[asset])
-		return amount;
-	return profileService.formatAmountWithUnit(amount, asset);
+  var formatAmountStr = function (amount, asset) {
+    if (!amount) return;
+    if (asset !== "base" && asset !== constants.BLACKBYTES_ASSET && !profileService.assetMetadata[asset])
+      return amount;
+    return profileService.formatAmountWithUnit(amount, asset);
   };
-	
-	var formatFeeStr = function(fee) {
-		if (!fee) return;
-		return fee + ' bytes';
-	};
 
-  root.processTx = function(tx) {
-    if (!tx) return; 
+  var formatFeeStr = function (fee) {
+    if (!fee) return;
+    return fee + ' bytes';
+  };
+
+  root.processTx = function (tx) {
+    if (!tx) return;
 
     var outputs = tx.outputs ? tx.outputs.length : 0;
     if (outputs > 1 && tx.action != 'received') {
       tx.hasMultiplesOutputs = true;
       tx.recipientCount = outputs;
-      tx.amount = lodash.reduce(tx.outputs, function(total, o) {
+      tx.amount = lodash.reduce(tx.outputs, function (total, o) {
         o.amountStr = formatAmountStr(o.amount, tx.asset);
         return total + o.amount;
       }, 0);
     }
-
+    tx.my_address = tx.addressFrom;
+    tx.asset = 'base';
+    tx.confirmations = tx.isStable;
+    tx.time = tx.creation_date;
     tx.amountStr = formatAmountStr(tx.amount, tx.asset);
     tx.feeStr = formatFeeStr(tx.fee || tx.fees);
 
