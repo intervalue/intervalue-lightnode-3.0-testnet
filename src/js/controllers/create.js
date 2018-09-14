9aa653d8d1dc44fcb6275efa7237719c3ddb43d0
ylwhlhp <ylwhlhp@126.com>
2018-09-14 16:56:56 +0800

    push

--- src/js/controllers/create.js
+++ src/js/controllers/create.js
@@ -1,213 +1,190 @@
 'use strict';
 
-angular.module('copayApp.controllers').controller('createController',
-    function($scope, $rootScope, $location, $timeout, $log, lodash, go, profileService, configService, isCordova, gettext, isMobile, derivationPathHelper, correspondentListService) {
-
+angular.module('copayApp.controllers').controller('createwalletController',
+    function ($rootScope, $scope, $timeout, storageService, notification, profileService, bwcService, $log) {
         var self = this;
-        var defaults = configService.getDefaults();
-        this.isWindowsPhoneApp = isMobile.Windows() && isCordova;
-        $scope.account = 1;
-
-        var defaults = configService.getDefaults();
-        $scope.derivationPath = derivationPathHelper.default;
-
-        // ng-repeat defined number of times instead of repeating over array?
-        this.getNumber = function(num) {
-            return new Array(num);
+        self.createwname = '';
+        self.createwpass = '';
+        self.createwrpass = '';
+        self.createwipass = '';
+        self.createwiname = '';
+        self.importcode = '';
+        self.createwirpass = '';
+        self.chosenWords = [];
+        self.showcodes = [];
+        self.showrandamcodes = [];
+        self.mnemonic = '';
+        self.showcodeerr = false;
+        self.createwalleterr = false;
+        self.showconfirm = false;
+        self.showtab = 'tabcold';
+        var fc = profileService.focusedClient;
+        var walletClient = bwcService.getClient();
+        self.ducodes = walletClient.createRandomMnemonic().split(' ');
+        self.passequal = function () {
+            if (self.createwpass !== self.createwrpass) {
+                self.createwalleterr = true;
+                return false;
+            } else {
+                self.step = "showcode";
+            }
         }
-
-        var updateRCSelect = function(n) {
-            $scope.totalCosigners = n;
-            self.RCValues = lodash.range(1, n + 1);
-            if ($scope.requiredCosigners > n || !$scope.requiredCosigners)
-                $scope.requiredCosigners = parseInt(n / 2 + 1);
+        //乱序
+        self.shuffle = function (v) {
+            for (var j, x, i = v.length; i; j = parseInt(Math.random() * i), x = v[--i], v[i] = v[j], v[j] = x);
+            return v;
         };
-
-        var updateSeedSourceSelect = function(n) {
-            self.seedOptions = [{
-                id: 'new',
-                label: gettext('New Random Seed')
-            }, {
-                id: 'set',
-                label: gettext('Specify Seed...')
-            }];
-            $scope.seedSource = self.seedOptions[0];
-        };
-
-        this.TCValues = lodash.range(2, defaults.limits.totalCosigners + 1);
-        $scope.totalCosigners = defaults.wallet.totalCosigners;
-        this.cosigners = [];//Array($scope.totalCosigners);
-        for (var i=0; i<$scope.totalCosigners-1; i++)
-            this.cosigners.push({});
-        correspondentListService.list(function(err, ab){
-            self.candidate_cosigners = ab;
-            $timeout(function(){
+        // 定义提示框内容
+        self.funReg = function () {
+            var newlist = [];
+            if (self.showrandamcodes.length > 3) {
+                // 显示乱序提示框
+                self.showrandamcodes = self.shuffle(JSON.parse(JSON.stringify(self.showrandamcodes)));
+                // 显示乱序提示框  结束
+                return false;
+            } else {
+                for (var i = 0; i <= 11; i++) {
+                    var newStr = {
+                        id: i,
+                        str: self.ducodes[i],
+                        chosen: false
+                    };
+                    newlist.push(newStr);
+                }
+                self.showcodes = JSON.parse(JSON.stringify(newlist));
+                self.showrandamcodes = self.shuffle(JSON.parse(JSON.stringify(newlist)));
+            }
+            $timeout(function () {
                 $scope.$digest();
             });
-        });
-
-        /*
-        $scope.$watch(function(){return $scope.totalCosigners;}, function(newVal, oldVal){
-            console.log("watch "+oldVal+" "+newVal);
-            if (newVal > oldVal)
-                for (var i=oldVal; i<newVal-1; i++)
-                    self.cosigners.push({});
-            else
-                self.cosigners.length = newVal-1;
-        }, true);*/
-
-        this.setTotalCosigners = function(tc) {
-            var oldLen = self.cosigners.length;
-            var newLen = tc-1;
-            if (newLen > oldLen)
-                for (var i=oldLen; i<newLen; i++)
-                    self.cosigners.push({});
-            else if (newLen < oldLen)
-                self.cosigners.length = newLen;
-
-            updateRCSelect(tc);
-            updateSeedSourceSelect(tc);
-            self.seedSourceId = $scope.seedSource.id;
         };
-
-        this.setMultisig = function(){
-            this.setTotalCosigners(3);
-            $scope.requiredCosigners = 2;
-        };
-
-        this.onCorrespondentSelected = function(device_address){
-            console.log(device_address);
-            if (device_address === "new")
-                go.path('correspondentDevices.addCorrespondentDevice');
-        };
-
-
-        this.setSeedSource = function(src) {
-            self.seedSourceId = $scope.seedSource.id;
-
-            $timeout(function() {
-                $rootScope.$apply();
-            });
-        };
-
-        function setError(error){
-            self.error = gettext(error);
+        // 定义提示框内容  结束
+        self.createwordf = function ($event) {
+            self.showcodeerr = false;
+            if ($event.srcElement.tagName == 'BUTTON') {
+                self.showrandamcodes.forEach(function (item, index) {
+                    if (item.id == $event.srcElement.id) {
+                        self.showrandamcodes[index].chosen = true;
+                        self.chosenWords.push({
+                            id: item.id,
+                            str: item.str
+                        })
+                    }
+                });
+            } else {
+                return false;
+            }
         }
-
-        this.create = function(form) {
-            if (form && form.$invalid) {
-                this.error = gettext('Please enter the required fields');
-                return;
+        self.minuswordf = function ($event) {
+            self.showcodeerr = false;
+            if ($event.srcElement.tagName == 'SPAN') {
+                self.showrandamcodes.forEach(function (item, index) {
+                    if (item.id == $event.srcElement.id) {
+                        self.showrandamcodes[index].chosen = false;
+                    }
+                });
+                self.chosenWords.forEach(function (item, index) {
+                    if (item.id == $event.srcElement.id) {
+                        self.chosenWords.splice(index, 1);
+                    };
+                })
+            } else {
+                return false;
             }
-            if (self.cosigners.length !== $scope.totalCosigners - 1)
-                return setError("invalid number of cosigners");
-
-            var opts = {
-                m: $scope.requiredCosigners,
-                n: $scope.totalCosigners,
-                name: form.walletName.$modelValue,
-                networkName: 'livenet',
-                cosigners: [],
-                isSingleAddress: $scope.isSingleAddress
-            };
-            if ($scope.totalCosigners > 1){
-                opts.cosigners = lodash.uniq(self.cosigners.map(function(cosigner){ return cosigner.device_address; }));
-                if (opts.cosigners.length !== $scope.totalCosigners - 1)
-                    return setError("Please select different co-signers");
-                for (var i=0; i<opts.cosigners.length; i++)
-                    if (!opts.cosigners[i] || opts.cosigners[i].length !== 33)
-                        return setError("Please fill all co-signers");
-            }
-            /*
-            var setSeed = self.seedSourceId == 'set';
-            if (setSeed) {
-
-                var words = form.privateKey.$modelValue || '';
-                if (words.indexOf(' ') == -1 && words.indexOf('prv') == 1 && words.length > 108)
-                    opts.extendedPrivateKey = words;
-                else
-                    opts.mnemonic = words;
-
-                opts.passphrase = form.passphrase.$modelValue;
-
-                var pathData = derivationPathHelper.parse($scope.derivationPath);
-                if (!pathData) {
-                    this.error = gettext('Invalid derivation path');
-                    return;
-                }
-
-                opts.account = pathData.account;
-                opts.networkName = pathData.networkName;
-                opts.derivationStrategy = pathData.derivationStrategy;
-
-            }
-            else
-                opts.passphrase = form.createPassphrase.$modelValue;
-
-            if (setSeed && !opts.mnemonic && !opts.extendedPrivateKey) {
-                this.error = gettext('Please enter the wallet seed');
-                return;
-            }
-            */
-
-            self._create(opts);
         };
+        $scope.$watch(function () {
+            return JSON.stringify(self.chosenWords);
+        }, function (newValue, oldValue) {
+            if (self.chosenWords.length > 11) {
+                var chostr = '';
+                for (var i = 0; i < self.chosenWords.length; i++) {
+                    chostr += self.chosenWords[i].id;
+                }
+                var showstr = '';
+                for (var i = 0; i < self.showcodes.length; i++) {
+                    showstr += self.showcodes[i].id;
+                }
+                if (chostr == showstr) {
+                    for (var i = 0; i < self.showcodes.length; i++) {
+                        self.mnemonic += ' ' + self.showcodes[i].str;
+                    }
+                    self.step = 'deletecode';
+                } else {
+                    self.showcodeerr = true;
+                }
+            }
+        }, true)
+        // 更改代码
+        self.haschoosen = function (noWallet) {
+            if (self.creatingProfile)
+                return console.log('already creating profile');
+            self.creatingProfile = true;
+            //	saveDeviceName();
 
+            $timeout(function () {
+                profileService.create({ noWallet: noWallet }, function (err) {
+                    if (err) {
+                        self.creatingProfile = false;
+                        $log.warn(err);
+                        self.error = err;
+                        $timeout(function () {
+                            $scope.$apply();
+                        });
+                        /*$timeout(function() {
+                            self.create(noWallet);
+                        }, 3000);*/
+                    }
+                });
+            }, 100);
 
-        this._create = function(opts) {
-            self.loading = true;
-            $timeout(function() {
-                profileService.createWallet(opts, function(err, walletId) {
+        };
+        // 删除口令 修改后
+        self.delteConfirm = function (walletName, passphrase, mnemonic) {
+            mnemonic = mnemonic.trim();
+            if (self.creatingProfile)
+                return console.log('already creating profile');
+            self.creatingProfile = true;
+            //	saveDeviceName();
+
+            $timeout(function () {
+                profileService.createWallet({ name: walletName, password: passphrase, mnemonic: mnemonic,m:1,n:1,networkName:"livenet",cosigners:[],isSinglecreateress:true }, function (err,walletId) {
                     self.loading = false;
                     if (err) {
                         $log.warn(err);
                         self.error = err;
-                        $timeout(function() {
+                        $timeout(function () {
                             $rootScope.$apply();
                         });
                         return;
                     }
 
-                    //if (opts.mnemonic || opts.externalSource || opts.extendedPrivateKey) {
-                    if (opts.externalSource) {
-                        if (opts.n == 1) {
-                            $rootScope.$emit('Local/WalletImported', walletId);
-                        }
+                    else {
+                        $rootScope.$emit('Local/WalletImported', walletId);
+                        $rootScope.createdataw = profileService.profile.credentials;
                     }
-                    /*if (opts.n > 1)
-                        $rootScope.$emit('Local/ShowAlert', "Please approve wallet creation on other devices", 'fi-key', function(){
-                            go.walletHome();
-                        });*/
+                });
+            }, 100);
+        };
+        //import wallet
+        self.importw = function(){
+            if (self.creatingProfile)
+                return console.log('already creating profile');
+            self.creatingProfile = true;
 
-                    if (opts.isSingleAddress) {
-                        profileService.setSingleAddressFlag(true);
+            $timeout(function () {
+                profileService.createWallet({ name: self.createwiname, password: self.createwipass, mnemonic: self.importcode,m:1,n:1,networkName:"livenet",cosigners:[],isSinglecreateress:true }, function (err) {
+                    if(err){
+                        self.creatingProfile = false;
+                        $log.warn(err);
+                        self.error = err;
+                        $timeout(function () {
+                            $scope.$apply();
+                        });
+                    }else{
+                        $rootScope.$emit('Local/WalletImported', walletId);
+                        $rootScope.createdataw = profileService.profile.credentials;
                     }
                 });
             }, 100);
         }
-
-        this.formFocus = function(what) {
-            if (!this.isWindowsPhoneApp)
-                return;
-
-            if (what && what == 'my-name') {
-                this.hideWalletName = true;
-                this.hideTabs = true;
-            } else if (what && what == 'wallet-name') {
-                this.hideTabs = true;
-            } else {
-                this.hideWalletName = false;
-                this.hideTabs = false;
-            }
-            $timeout(function() {
-                $rootScope.$digest();
-            }, 1);
-        };
-
-        $scope.$on("$destroy", function() {
-            $rootScope.hideWalletNavigation = false;
-        });
-
-        updateSeedSourceSelect(1);
-        self.setSeedSource('new');
     });
