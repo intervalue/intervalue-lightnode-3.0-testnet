8b7a9456e75142a95710a01d92fa069fa60701cd
molleahahs <github@1593576673@qq.com>
2018-10-10 17:08:22 +0800

    delete wallet

--- src/js/controllers/walletnamea.js
+++ src/js/controllers/walletnamea.js
@@ -1,9 +1,14 @@
 'use strict';
 
 angular.module('copayApp.controllers').controller('walletnameaController',
-    function($rootScope, $scope, $timeout, profileService, go, gettext, $state, $stateParams, storageService,lodash,$log) {
+    function($rootScope, $scope, $timeout, profileService, go, gettext, gettextCatalog, $state, $stateParams, isCordova, notification, profileService, storageService,lodash,$log) {
 
         var self = this;
+        self.showconfirm = false;
+        var delete_msg = gettextCatalog.getString('Are you sure you want to delete this wallet?');
+        var accept_msg = gettextCatalog.getString('Accept');
+        var cancel_msg = gettextCatalog.getString('Cancel');
+        var confirm_msg = gettextCatalog.getString('Confirm');
         self.walletId = $stateParams.walletId;
         self.name = $stateParams.name;
         self.image = $stateParams.image;
@@ -53,4 +58,39 @@ angular.module('copayApp.controllers').controller('walletnameaController',
                 }
             });
         }
+
+        //开始删除钱包
+        self.deleteWallet = function(walletId,name) {
+            if (profileService.profile.credentials.length === 1 || profileService.getWallets().length === 1)
+                return $rootScope.$emit('Local/ShowErrorAlert', "Can't delete the last remaining wallet");
+            if (isCordova) {
+                navigator.notification.confirm(
+                    delete_msg,
+                    function(buttonIndex) {
+                        if (buttonIndex == 1) {
+                            self.truedeleteWallet(walletId,name);
+                        }
+                    },
+                    confirm_msg, [accept_msg, cancel_msg]
+                );
+            } else {
+                self.showconfirm = true;
+            }
+        };
+
+        self.truedeleteWallet = function(walletId,name) {
+            var fc = profileService.focusedClient;
+            var walletName = (fc.alias || '') + ' [' + name + ']';
+            var self = this;
+
+            profileService.deleteWallet(walletId,name, function(err) {
+                if (err) {
+                    self.error = err.message || err;
+                } else {
+                    notification.success(gettextCatalog.getString('Success'), gettextCatalog.getString('The wallet "{{walletName}}" was deleted', {
+                        walletName: walletName
+                    }));
+                }
+            });
+        };
     });
