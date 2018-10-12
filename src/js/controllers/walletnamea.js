251ec86907750840b6b7e2c7cfb1d3e8df2afa5c
ylwhlhp <ylwhlhp@126.com>
2018-10-12 14:36:50 +0800

    translate 侧边栏，备份助记词后，显示错误

--- src/js/controllers/walletnamea.js
+++ src/js/controllers/walletnamea.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('copayApp.controllers').controller('walletnameaController',
-    function($rootScope, $scope, $timeout, profileService, go, gettext, gettextCatalog, $state, $stateParams, isCordova, notification, profileService, storageService,lodash,$log) {
+    function($rootScope, $scope, $timeout, profileService, go, gettext, gettextCatalog, $state, $stateParams, isCordova, notification, storageService,lodash,$log) {
 
         var self = this;
         self.showconfirm = false;
@@ -62,7 +62,7 @@ angular.module('copayApp.controllers').controller('walletnameaController',
         //开始删除钱包
         self.deleteWallet = function(walletId,name) {
             if (profileService.profile.credentials.length === 1 || profileService.getWallets().length === 1)
-                return $rootScope.$emit('Local/ShowErrorAlert', "Can't delete the last remaining wallet");
+                return $rootScope.$emit('Local/ShowErrorAlert', gettextCatalog.getString("Can't delete the last remaining wallet"));
             if (isCordova) {
                 navigator.notification.confirm(
                     delete_msg,
