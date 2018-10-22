cc439b55df3b8e168f51afa27cd44661f815106d
ylwhlhp <ylwhlhp@126.com>
2018-10-22 13:40:05 +0800

    初始化hub

--- angular-bitcore-wallet-client/bitcore-wallet-client/lib/api.js
+++ angular-bitcore-wallet-client/bitcore-wallet-client/lib/api.js
@@ -678,9 +678,12 @@ API.prototype.getTxHistory = function (asset, walletId, cb) {
 API.prototype.initDeviceProperties = function (xPrivKey, device_address, hub, deviceName) {
   console.log("initDeviceProperties");
   var device = require('intervaluecore/device.js');
+    var lightWallet = require('intervaluecore/light_wallet.js');
   if (device_address)
     device.setDeviceAddress(device_address);
   device.setDeviceName(deviceName);
+    device.setDeviceHub(hub);
+    lightWallet.setLightVendorHost(hub);
   //device.setDevicePrivateKey(Bitcore.HDPrivateKey.fromString(xPrivKey).derive("m/1'").privateKey.toBuffer());
 
   // since this is executed at app launch, give in to allow other startup tasks to complete
