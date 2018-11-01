3aa19987a5faace14d0ad461b2239ee823ce3755
molleahahs <github@1593576673@qq.com>
2018-11-01 11:44:38 +0800

    quick

--- src/js/controllers/index.js
+++ src/js/controllers/index.js
@@ -1959,7 +1959,7 @@ angular.module('copayApp.controllers').controller('indexController', function ($
             var date = new Date();
             var year = date.getFullYear();
             var month = date.getMonth() + 1;
-            var day = date.getDate();
+            var day = date.getDate() < 10 ? '0'+ date.getDate() :  date.getDate();
             var comprared = year + '-' + month + '-' + day;
             //计算星期几
             let qdatearr = datestr.split('-');
