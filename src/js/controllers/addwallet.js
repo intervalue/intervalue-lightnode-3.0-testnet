a5a1fe7931117953fb2591da873d0ebce5c95553
ylwhlhp <ylwhlhp@126.com>
2018-11-13 10:00:24 +0800

    注释，创建钱包等待画面

--- src/js/controllers/addwallet.js
+++ src/js/controllers/addwallet.js
@@ -144,13 +144,13 @@ angular.module('copayApp.controllers').controller('addwalletController',
                 return console.log('already creating profile');
             self.creatingProfile = true;
             //	saveDeviceName();
-            if (isCordova) {
-                window.plugins.spinnerDialog.show(null, gettextCatalog.getString('Loading...'), true);
-            }
+            // if (isCordova) {
+            //     window.plugins.spinnerDialog.show(null, gettextCatalog.getString('Loading...'), true);
+            // }
                 profileService.createWallet({ name: walletName, password: passphrase, mnemonic: mnemonic,m:1,n:1,networkName:"livenet",cosigners:[],isSinglecreateress:true  }, function (err,walletId) {
                    $timeout(function () {
-                       if (isCordova)
-                           window.plugins.spinnerDialog.hide();
+                       // if (isCordova)
+                       //     window.plugins.spinnerDialog.hide();
                        if (err) {
                            self.creatingProfile = false;
                            $log.warn(err);
@@ -188,9 +188,9 @@ angular.module('copayApp.controllers').controller('addwalletController',
 
             self.importcode1 = self.importcode.replace(/^\s+/, '').replace(/\s+$/, '');
             self.importcode2 = self.importcode1.replace(/\s+/g, ' ');
-            if (isCordova) {
-                window.plugins.spinnerDialog.show(null, gettextCatalog.getString('Loading...'), true);
-            }
+            // if (isCordova) {
+            //     window.plugins.spinnerDialog.show(null, gettextCatalog.getString('Loading...'), true);
+            // }
                 profileService.createWallet({ name: self.addwiname, password: self.addwipass, mnemonic: self.importcode2,m:1,n:1,networkName:"livenet",cosigners:[],isSinglecreateress:true }, function (err,walletId) {
                     $timeout(function () {
                         if (isCordova)
@@ -204,8 +204,8 @@ angular.module('copayApp.controllers').controller('addwalletController',
                                 $scope.$apply();
                             });
                         }
-                        if (isCordova)
-                            window.plugins.spinnerDialog.hide();
+                        // if (isCordova)
+                        //     window.plugins.spinnerDialog.hide();
                         $rootScope.$emit('Local/WalletImported', walletId);
                     });
 
